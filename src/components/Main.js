a3de0cb859e4c596e8c7961459ca5d98b423b4e0
deltavitor <cvitor1618@gmail.com>
2022-01-19 09:58:17 -0300

    Updated Main with styling and content

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,16 @@ import React from "react"
 
 function Main() {
   return (
-    <h1>Main content goes here</h1>
+    <main className="main">
+      <h1 className="main--title">Fun facts about React</h1>
+      <ul className="main--unordered_list">
+        <li className="main--list_item">Was first released in 2013</li>
+        <li className="main--list_item">Was originally created by Jordan Walke</li>
+        <li className="main--list_item">Has well over 100K stars on GitHub</li>
+        <li className="main--list_item">Is maintained by Facebook</li>
+        <li className="main--list_item">Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
   )
 }
 
