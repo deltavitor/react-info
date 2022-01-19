e50d24676f1b59c501c63d07173f36e5af7b35ed
deltavitor <cvitor1618@gmail.com>
2022-01-19 08:14:32 -0300

    Created App component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Navbar from "./components/Navbar.js"
+import Main from "./components/Main.js"
+
+function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <Main />
+    </div>
+  )
+}
+
+export default App
