7af032a18a52536f32ef63f2f9978879d0bc34e5
deltavitor <cvitor1618@gmail.com>
2022-01-19 09:24:40 -0300

    Updated Navbar with styling and content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,14 @@
 import React from "react"
 
+import reactLogo from "../images/reactjs-icon.svg"
+
 function Navbar() {
   return (
-    <h1>Navbar goes here</h1>
+    <nav className="navbar">
+      <img src={reactLogo}  className="navbar--icon" alt="React logo"/>
+      <h3 className="navbar--logo">ReactFacts</h3>
+      <h4 className="navbar--title">React Course - Project 1</h4>
+    </nav>
   )
 }
 
